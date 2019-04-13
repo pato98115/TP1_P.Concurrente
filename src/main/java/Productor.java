f6d0d9cf979638f2c3937d624b1d21d852cf71ca
pato98115 <44127015+pato98115@users.noreply.github.com>
2019-04-13 17:54:21 -0300

    Productor y consumidor
    borrado de clases que se pisaban y agregado cuerpo al productor y consumidor

--- src/main/java/Productor.java
+++ src/main/java/Productor.java
@@ -1,3 +1,16 @@
-public class Productor {
+public class Productor implements Runnable{
 
+    private Buffer buffer;
+    private final int CANT_A_PRODUCIR = 100;
+
+    public Productor(Buffer buffer){ //crea un Producctor y le asigna un buffer
+        this.buffer = buffer;
+    }
+
+    @Override
+    public void run(){
+        for(int i=0; i<CANT_A_PRODUCIR; i++){
+            buffer.agregar();
+        }
+    }
 }
